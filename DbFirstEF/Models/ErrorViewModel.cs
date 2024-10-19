6d1c04dfa4e0335f3f959fef138a4032e73b8a0b
abedcodes13 <muhdabed21@gmail.com>
2024-10-19 23:19:19 +0930

    Delete DbFirstEF directory

--- DbFirstEF/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DbFirstEF.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
